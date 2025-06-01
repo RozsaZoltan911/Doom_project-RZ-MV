23969857f9edeb2f5ab5eb6dc65bcaafbdc8a3e5
vektor32 <167175808+vektor32@users.noreply.github.com>
2025-06-01 22:27:27 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(6);
+  max = Math.floor(1);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  const motd1 = document.getElementById("motd1")
+  const motd2 = document.getElementById("motd2")
+  const motd3 = document.getElementById("motd3")
+  const motd4 = document.getElementById("motd4")
+  const titles = document.querySelectorAll(".motdtitle"); 
+
+
+  console.log(getRandomInt(1, 6));
+  let motd = getRandomInt(1, 6)
+  if (motd === 2){
+    titles.forEach(title => {
+      title.classList.remove("show")});
+    motd1.classList.add("show")
+  } else if (motd === 3){
+    titles.forEach(title => {
+      title.classList.remove("show")});
+    motd2.classList.add("show")
+  } else if (motd === 4){
+    titles.forEach(title => {
+      title.classList.remove("show")});
+    motd3.classList.add("show")
+  } else{
+    titles.forEach(title => {
+      title.classList.remove("show")});
+    motd4.classList.add("show")
+  }
+});
+
+
+
+function openNav(){
+  const menuicon = document.querySelector(".menuicon")
+  const menucloseicon = document.querySelector(".menucloseicon")
+  const mobile = document.querySelector(".mobile")
+  menuicon.style.display = 'none'
+  mobile.style.display = 'flex'
+  menucloseicon.style.display = 'flex'
+};
+function closeNav(){
+  const menuicon = document.querySelector(".menuicon")
+  const menucloseicon = document.querySelector(".menucloseicon")
+  const mobile = document.querySelector(".mobile")
+  menuicon.style.display = 'flex'
+  mobile.style.display = 'none'
+  menucloseicon.style.display = 'none'
+
+};
